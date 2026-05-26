69f681a5d6b0f44a8103ede4d23deabcd7f286f5
Saikrishna19-sk <rapakasaikrishna7@gmail.com>
2026-05-26 09:00:56 +0530

    remove sentence-transformers to fix memory

--- app/services/reranker.py
+++ app/services/reranker.py
@@ -1,18 +1,3 @@
-from sentence_transformers import CrossEncoder
-
 class Reranker:
-    def __init__(self):
-        self.model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
-
     def rerank(self, query, results):
-        pairs = [
-            (query, r[1]["text"])
-            for r in results
-        ]
-
-        scores = self.model.predict(pairs)
-
-        reranked = list(zip(scores, [r[1] for r in results]))
-        reranked.sort(reverse=True, key=lambda x: x[0])
-
-        return reranked
+        return results
